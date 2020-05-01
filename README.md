7399cc9fdeba41a63d1bff4881aff4b23a6c3e01
Rob Roschewsk <pabutusa@gmail.com>
2020-05-01 13:54:00 -0400

    Added --debug option. Also added code to null out FFT results outside of the 300-2800HZ bandwidth

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # wave2ttd
 Experiments in Two Tone decoding
+
+This program couples with MULTIRX of any other program that can record
+two tones pages to a wave file.
+
+Theory of operation:
+
+Build an array of the tone sequence to be detected in 200ms chunks. For example
+a tone sequence of 400Hz x 1sec + 200Hz x 2 sec looks like:
+[200,200,200,200,200,400,400,400,400,400,400,400,400,400,400]
+
+Read the wav file in 200ms chunks (wav framerate * 0.2 bytes)
+
+Pack the byte data into an array of shorts (16 bit) (so for now it only works on 16bit wavs)
+
+Perform an FFT on the chunk, find the largest 'bin', calculate the frequency, and append that to a buffer. The buffer acts a a "sliding window" of the incoming wav. As we add values to the end, we delete values from the start and maintain a constant size buffer, in the current case the buffer is 40 values long or about 8 seconds.
+
+Compare the saved tone sequence to the same number of values at the end of the buffer.
+ 
+If we match, decalare success and print the detect tone. Otherwise loop and grab another wav sample and try again.
+
+Continue until all of the frames in the wav file have ben consumed. If no tone sets were detected print "UNKNOWN"
+
+
--- wave2ttd.py
+++ wave2ttd.py
@@ -8,9 +8,10 @@ from numpy import zeros, short, fromstring, array, chararray
 from numpy.fft import fft
 from collections import defaultdict
 from functools import reduce
+import argparse
 import csv
 
-def main(file):
+def main(args):
    ToneDict = defaultdict(list)
    buffer = [0] * 40
 
@@ -20,21 +21,23 @@ def main(file):
          ttemp = [float(row[1])] * int(round(float(row[2]) / 0.2)) + [float(row[3])] * int(round(float(row[4]) / 0.2))
          ToneDict[row[0]] = array(ttemp)
 
-   f=wave.open(file,'rb')
-   numFrames=f.getnframes()
-   #print(f"frames: {numFrames}")
-   #print(f"channels: {f.getnchannels()}")
-   #print(f"width: {f.getsampwidth()}")
-   rate=f.getframerate()
-   #print(f"rate: {rate}")
+   if args.debug:
+       debug = open('debug.csv', 'w', newline='')
+       debugwriter=csv.writer(debug)
 
-   # fetch audio in 200ms chunks
-   chunk=int(rate*0.2)
-   #print(f"chunk size: {chunk}")
+   f = wave.open(args.filename,'rb')
+   numFrames = f.getnframes()
+   rate = f.getframerate()
+   chunk = int(rate*0.2) # fetch audio in 200ms chunks
+
+   if args.debug:
+       print("frames: {0} channels: {1}".format(numFrames, f.getnchannels()))
+       print("width: {0} rate: {1} chuck size:{2}".format(f.getsampwidth(), rate, chunk))
+
 
    found = 0
    w=f.readframes(chunk)
-   while len(w) > 0:
+   while len(w) >= chunk:
        audio_data=list(unpack(str(int(len(w)/2))+'h',w))
        audio_threshold=50
        tone_offset=0
@@ -42,10 +45,15 @@ def main(file):
            window = np.hamming(len(audio_data))
            audio_data = audio_data * window
            FFT = abs(np.fft.rfft(audio_data))
-           FFT[:46] = 0
-           FFT[559:] = 0
+           lo_limit = int((300*len(FFT))/(rate/2))
+           hi_limit = int((2800*len(FFT))/(rate/2))
+           FFT[:lo_limit] = 0
+           FFT[hi_limit:] = 0
            freq = rate / 2 * FFT.argmax() / len(FFT) * (1 + tone_offset)
-           print(f"freq: {freq}")
+           if args.debug:
+               print("freq: {0:8.2f} len(FFT): {1} FFT.argmax: {2:>3d} lo_limit: {3} hi_limit: {4}"
+                     .format(freq, len(FFT), FFT.argmax(), lo_limit, hi_limit))
+               debugwriter.writerow(FFT[lo_limit:hi_limit])
            buffer.append(freq)
            buffer.pop(0)
            for tonedescription in ToneDict:
@@ -55,13 +63,21 @@ def main(file):
               temp2 = temp > 100000
               temp = operator.or_(temp1, temp2)
               if reduce(operator.and_, temp):
-                 print(f"{tonedescription}")
+                 print(tonedescription)
                  found = 1
        w=f.readframes(chunk)
+
    if found == 0:
        print("UNKNOWN")
    f.close()
+   if args.debug:
+       debug.close()
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Decode wav file with TwoTone signals.')
+    parser.add_argument("filename", help="input wav file")
+    parser.add_argument("-d", "--debug", help="debug mode", action="store_true")
+    args = parser.parse_args()
+
+    main(args)
 
